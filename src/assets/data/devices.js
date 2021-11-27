1c6a43e50ae9f9f2865d8c3228240d745a6c626a
Luis Gabriel <janco7249@gmail.com>
2021-11-27 00:11:14 -0400

    feat: add icons components

---
+++ src/assets/data/devices.js
@@ -0,0 +1,38 @@
+export const devices = [
+  {
+    img: "/images/desktop.png",
+    title: "Tv",
+    description: [
+      "Amazon Fire Tv",
+      "Android TV",
+      "Apple TV",
+      " Chromecast  LG TVs",
+      "Samsung",
+    ],
+  },
+  {
+    img: "/images/laptop.png",
+    title: "Computer",
+    description: ["Chrome OS", "MacOS", "Windows PC"],
+  },
+  {
+    img: "/images/mobile.png",
+    title: "Mobile & Tablet",
+    description: [
+      "Amazon Fire Tablets",
+      "Android Phones & Tablets",
+      "iPhone & iPad",
+    ],
+  },
+  {
+    img: "/images/console.png",
+    title: "Game Consoles",
+    description: [
+      "PS4",
+      "PS5",
+      "Xbox One",
+      "Xbox Series X",
+      "Xbox Series S",
+    ],
+  },
+];
