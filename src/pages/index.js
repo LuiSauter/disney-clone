74e7bc4fe0972bcd6c5075f6c70d0f99d3154e22
Luis Gabriel <janco7249@gmail.com>
2021-11-27 00:11:42 -0400

    feat: index page security

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import { FooterEnd } from "../components/Footer/FooterEnd";
+import Header from "../components/Header/Header";
+import Login from "../components/Login/Login";
+
+export default function index() {
+  return (
+    <>
+      <Header />
+      <Login />
+      <FooterEnd />
+    </>
+  );
+}
