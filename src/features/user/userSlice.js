22dc36f32b7fc4ec24887d46e36301491e5402b6
Luis Gabriel <janco7249@gmail.com>
2021-11-28 22:17:42 -0400

    refactor: add uid

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -4,6 +4,7 @@ const initialState = {
   name: "",
   email: "",
   photo: "",
+  uid: ''
 };
 
 const userSlice = createSlice({
@@ -14,11 +15,13 @@ const userSlice = createSlice({
       state.name = action.payload.name;
       state.email = action.payload.email;
       state.photo = action.payload.photo;
+      state.uid = action.payload.uid;
     },
     setSignOutState: (state) => {
       state.name = null;
       state.email = null;
       state.photo = null;
+      state.uid = null;
     },
   },
 });
@@ -26,5 +29,6 @@ export const { setUserLoginDetails, setSignOutState } = userSlice.actions;
 export const selectUserName = state => state.user.name
 export const selectUserEmail = state => state.user.email
 export const selectUserPhoto = state => state.user.photo
+export const selectUserUid = state => state.user.uid
 
 export default userSlice.reducer
