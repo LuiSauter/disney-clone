1723705fadbac4966df6ee65e9cd1b97f4e44d70
Luis Gabriel <janco7249@gmail.com>
2021-11-29 23:23:24 -0400

    refactor: presentation page

---
+++ src/pages/HomePage.js
@@ -0,0 +1,6 @@
+import Home from "../components/Home/Home";
+
+export default function HomePage() {
+  return <Home />;
+}
+
--- src/pages/PresentationPage.js
+++ src/pages/PresentationPage.js
@@ -1,13 +1,17 @@
 import styled from "styled-components";
 import Presentation from "../components/Presentation/Presentation";
 import { useUser } from "../hooks/useUser";
+import { FooterEnd } from '../components/Footer/FooterEnd'
 
 export default function PresentationPage() {
   const { userLogget } = useUser();
   return (
     <>
       {userLogget === null ? (
-        <Presentation />
+        <>
+          <Presentation />
+          <FooterEnd />
+        </>
       ) : (
         <>
           <LoaderDisney>
