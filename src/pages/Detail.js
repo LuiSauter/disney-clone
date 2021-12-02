ba6a51b2f9a0e03fcf39150152b67d26939317f5
Luis Gabriel <janco7249@gmail.com>
2021-12-02 18:07:59 -0400

    nothing

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,4 +1,10 @@
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import { selectAllMovies } from "../features/movie/movieSlice";
 export default function Detail() {
+  const { id } = useParams();
+  const allMovies = useSelector(selectAllMovies)
+  // console.log(allMovies.includes(movie => id));
   return (
     <div>
       <h2>Hola</h2>
